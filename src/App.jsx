32c73f9846f6f066c6c4b22eb7e01094b6787234
lyDiyamong <diyamong26@gmail.com>
2024-07-19 22:22:36 +0700

    add title

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import { useState } from 'react'
 import './index.css'
+import BookCreate from './components/BookCreate'
 
 function App() {
   const [books, setBooks] = useState([])
+
+  const createBook = (title) => {
+    console.log('Need to add book with:', title)
+  }
   return (
-    <div className='text-2xl'>App</div>
+    <div className='text-2xl'>
+        <BookCreate onCreate={createBook}/>
+    </div>
   )
 }
 
--- src/components/BookCreate.jsx
+++ src/components/BookCreate.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import { useState } from 'react'
+import '../index.css'
+function BookCreate({onCreate}) {
+  	const [title, setTitle] = useState('')
+	
+	const handleChange = (event) => {
+		setTitle(event.target.value)
+		// to capture each of title change cuz title we set it as empty string
+	}
 
-function BookCreate() {
-  return (
-    <div>BookCreate</div>
-  )
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		onCreate(title)
+		setTitle('')
+	}
+	return (
+		<div className='bg-blue-950 text-white p-[15px]'>
+			<h3 className='font-bold'>Add a Book</h3>
+			<form onSubmit={handleSubmit} className='flex flex-col gap-3' >
+				<label >Title:</label>
+				<input value={title} onChange={handleChange} className=' border border-slate-300 w-[400px] rounded-lg' />
+				<button className='p-3 bg-blue-300 w-[100px] rounded-md' >Create!</button>
+			</form>
+		</div>
+		
+	)
 }
 
 export default BookCreate
