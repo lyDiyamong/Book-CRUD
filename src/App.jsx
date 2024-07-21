1b95916b64c88cc17b8d714054d21f07e2868e76
lyDiyamong <diyamong26@gmail.com>
2024-07-21 14:46:54 +0700

    CRUD with api method

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from 'axios'
 import "./index.css";
 import BookCreate from "./components/BookCreate";
 import BookList from "./components/BookList";
@@ -10,30 +11,53 @@ function App() {
 	react won't update to the app component if they compare the original state 
 	and the update state have the same reference, so react won't re-render
 	*/
+	// get all of the books in array
+	const fetchBooks = async () => {
+		// response is a new array
+		const response = await axios.get('http://localhost:5174/books')
+		setBooks(response.data)
+	}
+
+	// useEffect
+	useEffect(() => {
+		fetchBooks();
+	}, [])
+
 	// edit
-	const editBookById = (id, newTitle) => {
+	const editBookById = async (id, newTitle) => {
+		const response = await axios.put(`http://localhost:5174/books/${id}`, {
+			title: newTitle
+		})
+		
 		const updatedBooks = books.map((book) => {
 			if (book.id === id) {
-				return {... book, title : newTitle};
+				return {... book, ...response.data};
 			}
 			return book
 		})
 		setBooks(updatedBooks)
 	}
 	// create
-	const createBook = (title) => {
+	const createBook = async (title) => {
+		const response = await axios.post('http://localhost:5174/books' , {
+			title
+		})
+		
 		const updateBooks = [
 			...books,
-			{ id: Math.floor(Math.random() * 10000), title: title },
+			response.data
+			// { id: Math.floor(Math.random() * 10000), title: title },
 		];
 		/* 
 		if using concat or spread operator it will be add to new address every times
 		*/
 		setBooks(updateBooks);
-		console.log(updateBooks);
+		
+		
 	};
 	// delete
-	const deleteBookById = (id) => {
+	const deleteBookById = async (id) => {
+		await axios.delete(`http://localhost:5174/books/${id}`)
 		const updateBooks = books.filter(book => {
 			return book.id !== id
 		})
